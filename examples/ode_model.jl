11d5699b04d80b0ddfb16370b472dc9f657c3819
Joseph <joseph03choi@gmail.com>
2025-10-06 12:08:46 -0400

    trying to debug kinetic IE formulation

--- examples/ode_model.jl
+++ examples/ode_model.jl
@@ -42,7 +42,7 @@ intensity(x_A,x_B,x_D) = x_A + 2/21*x_B + 2/21*x_D
 
 using Ipopt
 @mtkcompile o = KineticParameterEstimation()
-model = Model(optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-6))
+model = Model(optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-4))
 decision_vars(o) # Displays: x_Z(t), x_Y(t), x_D(t), x_B(t), x_A(t), k_2f, k_3f, k_4
 # FIRST, create discretized state decision variables
 N = Int(floor((tspan[2] - tspan[1])/tstep))+1
@@ -60,19 +60,33 @@ println("TIME: $(round.(JuMP.solve_time(model),digits=6))")
 println("f^* = $(round(JuMP.objective_value(model),digits=6))")
 println("p* = $(round.(JuMP.value.(p),digits=5)).")
 
-using EAGO
+using EAGO, Gurobi
 @mtkcompile o = KineticParameterEstimation()
-model = Model(optimizer_with_attributes(EAGO.Optimizer, "relative_tolerance" => 1e-6)) # NEED TO FIX HERE
+factory = () -> EAGO.Optimizer(SubSolvers(; r = Gurobi.Optimizer()))
+model = Model(factory)
+# trying a bunch of different settings, this at least allows it to run for a bit
+set_attribute(model, "time_limit", 3600*24.0)
+set_attribute(model, "relative_tolerance", 1e-4)
+set_attribute(model, "output_iterations", 1)
+set_attribute(model, "obbt_repetitions", 5)
+set_attribute(model, "fbbt_lp_repetitions", 5)
+set_attribute(model, "branch_cvx_factor", 0.5)
+set_attribute(model, "absolute_constraint_feas_tolerance", 1e-5)
+set_attribute(model, "branch_pseudocost_on", true)
+set_attribute(model, "reverse_subgrad_tighten", true)
+set_attribute(model, "cut_max_iterations", 12)
+set_attribute(model, "output_iterations", 50)
+# set_attribute(model, "verbosity", 2)
 decision_vars(o) # Displays: x_Z(t), x_Y(t), x_D(t), x_B(t), x_A(t), k_2f, k_3f, k_4
 # FIRST, create discretized state decision variables
 N = Int(floor((tspan[2] - tspan[1])/tstep))+1
 V = length(unknowns(o))
-@variable(model, -75 <= z[1:V,1:N] <= 150.0 ) # ̇z = (x_Z(t), x_Y(t), x_D(t), x_B(t), x_A(t))
+@variable(model, -70 <= z[1:V,1:N] <= 150.0 ) # ̇z = (x_Z(t), x_Y(t), x_D(t), x_B(t), x_A(t))
 # SECOND, create free design decision variables
 pL = [10, 10, 0.001]
 pU = [1200, 1200, 40]
 @variable(model, pL[i] <= p[i=1:3] <= pU[i]) # p = (k_2f, k_3f, k_4)
-register_odesystem(model, o, tspan, tstep, "EE")
+register_odesystem(model, o, tspan, tstep, "IE")
 @objective(model, Min, sum((intensity(z[5,i],z[4,i],z[3,i]) - data[i-1])^2 for i in 2:N))
 JuMP.optimize!(model)
 println("STATUS: $(JuMP.termination_status(model)), RESULT CODE: $(JuMP.primal_status(model))")
